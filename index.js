a3cc6a4f20c76279ae8bca3c9cfef9291f20e11f
Lee <leeirungu11313@gmail.com>
2024-03-28 08:51:50 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector("main")
+main.remove()
+const YOUR_NAME = "Lee"
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = `${YOUR_NAME} is the champion`
